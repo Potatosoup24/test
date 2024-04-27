19144c87047ddcce4cd97e84ebc7fca150106067
potatosoup24 <sunjoong1110@gmail.com>
2024-04-27 10:43:45 +0900

    adding gugudan

--- index.c
+++ index.c
@@ -3,8 +3,11 @@
 int main(void)
 {
 	printf("Github testing");
-	for (int i = 1; i < 10; i++) {
-		printf("%d", i);
+	for (int i = 2; i < 10; i++) {
+		printf("%d´Ü\n", i);
+		for (int j = 1; j < 10; j++) {
+			printf("\t%d X %d = %d\n", i,j,i*j);
+		}
 	}
 	return 0;
 }
